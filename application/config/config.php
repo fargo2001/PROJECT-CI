25a4c3d6ef11cfedd4dd509d281f96eaf3df61b0
andreastan <swtdehkk@yahoo.com>
2015-02-25 16:55:59 +0700

    update htaccess dan config base url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '/codeigniter';
+$config['base_url']	= '/PROJECT-CI';
 
 /*
 |--------------------------------------------------------------------------
